0e34d89783e954333a3f9dbc74719d0d8dd8ce1b
adrian <stroeadrian85@yahoo.com>
2021-04-26 16:32:02 +0300

    Co-authored-by: Neata Alexandru <LordHarkon@users.noreply.github.com>

--- server/index.js
+++ server/index.js
@@ -24,6 +24,7 @@ app.use('/', require('./routes/products'));
 app.use(require('./middleware/notFound'));
 app.use(require('./middleware/errorHandler'));
 
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
